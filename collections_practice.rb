ca27877cd9fb0cf208f5371f4dfc38c9d7aeafb8
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-19 01:29:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,35 @@
-def sort_array_asc
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count
   
 end
 
-def sort
+def swap_elements
+
+end
+  
+def reverse-array
+
+end
+
+def kesha_maker
+  
+end
+
+def find_a
+  
+end
+
+def sum_array
+  
+end
+
+def add_s
+  
+end  
